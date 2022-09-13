0b55bf17c78596a9b80f6335ce26aa9d394cb2b2
Ikemefuna Jesse <jesseikemefuna@gmail.com>
2022-09-13 14:13:52 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * print_alphabet_10x - function to print abc 10 times
+ * Description:  print_alphabet_10x function prints abc 10 times
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
 	char c, i;
 
-	for (i =0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
